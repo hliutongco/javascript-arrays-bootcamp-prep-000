e9cab3744f1a9ac7a4d9d4f84d8466b610c43d64
Helen Liutongco <bellpickle@gmail.com>
2017-10-24 04:04:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,14 +1,11 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
-/*function addElementToBeginningOfArray(input) {
-  var array=[];
+function addElementToBeginningOfArray(array, input) {
   (...array, input)
   return array;
 }
 
-function destructivelyAddElementToBeginningOfArray(input) {
-var array=[];
+function destructivelyAddElementToBeginningOfArray(array, input) {
   array.unshift(input);
   return array;
 }
-*/
