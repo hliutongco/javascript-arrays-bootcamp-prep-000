0d91ceb2383954cdc15308b254184d4cd4087b5c
Helen Liutongco <bellpickle@gmail.com>
2017-10-24 04:12:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
-function addElementToBeginningOfArray(array, element) {
-  return (...array, element)
-}
+//function addElementToBeginningOfArray(array, element) {
+//  return (...array, element)
+//}
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   return array.unshift(element);
