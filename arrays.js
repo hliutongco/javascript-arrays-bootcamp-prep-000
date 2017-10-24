e4244f61455b7405c84c74dc74cae2753ff71f64
Helen Liutongco <bellpickle@gmail.com>
2017-10-24 04:02:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,5 @@
-var chocolateBars=["snickers","hundred grand","kitkat","skittles"];
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+
 function addElementToBeginningOfArray(input) {
   var array=[];
   (...array, input)
